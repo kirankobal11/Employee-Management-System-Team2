cd78ced07cc99e5bc78804956bf44690690925cb
MuraliKrishnaVHegde <muraliihegde2002@gmail.com>
2024-09-26 16:51:05 +0530

    Update DepartmentService.java

--- src/main/java/com/deloitte/demo/service/DepartmentService.java
+++ src/main/java/com/deloitte/demo/service/DepartmentService.java
@@ -1,33 +1,30 @@
 package com.deloitte.demo.service;
 
+import java.util.List;
 import com.deloitte.demo.model.Department;
 import com.deloitte.demo.repository.DepartmentRepository;
 
-import javax.inject.Inject;
-import java.util.List;
-
 public class DepartmentService {
 
-    @Inject
-    private DepartmentRepository departmentRepository;
+	private DepartmentRepository departmentRepository = new DepartmentRepository();
 
-    public void createDepartment(Department department) {
-        departmentRepository.saveDepartment(department);
-    }
+	public Department addDepartment(Department department) {
+		return departmentRepository.addDepartment(department);
+	}
 
-    public Department getDepartmentById(int id) {
-        return departmentRepository.findDepartmentById(id);
-    }
+	public List<Department> getAllDepartments() {
+		return departmentRepository.getAllDepartments();
+	}
 
-    public List<Department> getAllDepartments() {
-        return departmentRepository.findAllDepartments();
-    }
+	public Department getDepartmentById(int id) {
+		return departmentRepository.getDepartmentById(id);
+	}
 
-    public void updateDepartment(Department department) {
-        departmentRepository.updateDepartment(department);
-    }
+	public void updateDepartment(Department department) {
+		departmentRepository.updateDepartment(department);
+	}
 
-    public void deleteDepartment(int id) {
-        departmentRepository.deleteDepartment(id);
-    }
+	public void deleteDepartment(int id) {
+		departmentRepository.deleteDepartment(id);
+	}
 }
